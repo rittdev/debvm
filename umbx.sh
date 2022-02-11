33b3eb3df8abd96e82001ed633c5ddf2b07c1bc9
Steve Rittmeyer <steve@rittdev.com>
2022-02-11 10:25:27 -0500

    added umbx.sh

---
+++ umbx.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# base
+
+apt-get -y install --no-install-recommends \
+	logrotate \
+	vim \
+	bzip2 \
+	openssh-server \
+	curl \
+	ca-certificates
+
+# xfce
+
+apt-get -y install --no-install-recommends \
+	xorg \
+	xfwm4 \
+	xfdesktop4 \
+	xfce4-session \
+	thunar \
+	xfce4-terminal \
+	shiki-colors-xfwm-theme \
+	greybird-gtk-theme \
+	blackbird-gtk-theme
+
+# sound
+
+apt-get -y install --no-install-recommends \
+	alsa-utils
+
+# vmware support
+
+apt-get -y install --no-install-recommends \
+	open-vm-tools-desktop
+
+apt-get -y install --no-install-recommends \
+	xserver-xorg-video-vmware
+
+# web and pdf
+
+apt-get -y install --no-install-recommends \
+	libdbus-glib-1-2
+
+# firefox support
+apt-get -y install --no-install-recommends \
+	libgtk-3-common \
+	libgtk-3-bin \
+	pulseaudio
+
+apt-get -y install --no-install-recommends \
+	ffmpeg
+
+# config normal user
+
+curl -O https://rittdev.com/debvm/config.sh
+
+mv config.sh /home/ritt/config.sh
+
+chmod a+x /home/ritt/config.sh
+
+chown ritt:ritt /home/ritt/config.sh
+
+su - ritt -c /home/ritt/config.sh
